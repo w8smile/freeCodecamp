0e91b36ec037e30ae82072a61f60b27ff765a19d
w8smile <w8smile@gmail.com>
2023-10-20 12:55:26 +0300

    Изучите различия между ключевыми словами var и let

---
+++ JavaScript Algorithms and Data Structures/8. Изучите различия между ключевыми словами var и let.js
@@ -0,0 +1,7 @@
+// var camper = "James";
+// var camper = "David";
+// console.log(camper);
+// В приведенном выше коде camper переменная изначально объявляется как James, а затем переопределяется на David. Затем консоль отображает строку David.
+
+let catName = "Oliver";
+let catSound = "Meow!";
