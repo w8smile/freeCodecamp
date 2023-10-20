f69cb9e1acffab848f31288ed465b44a5e6ed7f7
w8smile <w8smile@gmail.com>
2023-10-20 12:52:47 +0300

    Понимание учета регистра в переменных

---
+++ JavaScript Algorithms and Data Structures/7. Понимание учета регистра в переменных.js
@@ -0,0 +1,15 @@
+// Напишите имена переменных в JavaScript в CamelCase.
+// В CamelCase имена переменных, состоящие из нескольких слов, имеют первое слово в нижнем регистре, а первая буква каждого последующего слова пишется с заглавной буквы.
+//
+// Примеры:
+// var someVariable;
+// var anotherVariableName;
+// var thisVariableNameIsSoLong;
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
